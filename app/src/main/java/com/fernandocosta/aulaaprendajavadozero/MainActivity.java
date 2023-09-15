0e6640a5218a43cc0dacb4f59838d02a1d5e575c
Fernando Costa <contact.fernandocosta@gmail.com>
2023-09-14 23:22:23 -0300

    Hello  World!

---
+++ app/src/main/java/com/fernandocosta/aulaaprendajavadozero/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fernandocosta.aulaaprendajavadozero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
